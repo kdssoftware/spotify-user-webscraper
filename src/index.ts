3f3aa878ba27429a29cdaa3ff8a716b50c5804a0
Karel De Smet <snakehead007@pm.me>
2021-05-21 14:14:07 +0200

    update to fix chars and date

--- src/index.ts
+++ src/index.ts
@@ -95,14 +95,13 @@ const searchNextUser = (username:string|number, page:puppeteer.Page):Promise<any
       following,
       profilename,
       followers,
-      lastUpdated:Date.now(),
+      lastUpdated:Date(),
       link:"https://open.spotify.com/user/"+username
     });
 })}
 
-
 const increaseUsername = (username:string) =>{
-  const chars:string[] = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','%','_','-','&','*',')','(','=',',','^','@','~',':','>','<','|',']','[','+','$','!'];
+  const chars:string[] = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','%','_','-','&','*',')','(','=',',','^','@','~',':','>','<','|',']','[','+','$','!'];
   /**
    * 0 -> = -> 00 -> == -> 000 -> === -> .......
    */
